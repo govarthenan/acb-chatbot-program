99b625aa362b133321a9e493b52af2e70b06eaaa
Govarthenan Rajadurai <govarthenan@gmail.com>
2026-02-11 17:01:19 +0530

    feat: Implement file upload to LLM

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+from pathlib import Path
 
 import httpx
 from dotenv import load_dotenv
@@ -10,7 +11,7 @@ load_dotenv("./.env")
 openrouter_secret = os.getenv("OPENROUTER_SECRET")
 
 # message history container
-message_history = []
+message_history: list[dict[str:str]] = []
 
 # message history structure
 """
@@ -20,6 +21,47 @@ message_history = []
 {role: user, content: <prompt>}
 ]
 """
+
+# list of files to be uploaded, refreshed at each turn of the conversation
+file_upload_paths: list[str] = []
+
+
+def preprocessor_txt(txt_file_path: str) -> str:
+    with open(file=txt_file_path, mode="r") as f:
+        content = f.read()
+
+    return content
+
+
+def files_handler(file_list: list[str]) -> dict[str:str]:
+    file_contents: dict[str:str] = {}
+
+    # check if given files exist
+    for current_path in file_list:
+        if os.path.isfile(path=current_path):
+            file_type = Path(current_path).suffix
+
+            # knwon filetypes: txt
+            if file_type == ".txt":
+                current_file_content: str = preprocessor_txt(current_path)
+                file_contents[current_path] = current_file_content
+            else:
+                print(f"\nWARN: File type {file_type} not supported!\n")
+        else:
+            print(f"\nWARN: File {current_path} not found!\n")
+
+    return file_contents
+
+
+def file_content_prompt_generator(file_contents: dict[str:str]) -> str:
+    prompt_starter = "The user has uploaded some files. Here are the contents..."
+
+    for key, val in file_contents.items():
+        prompt_starter: str = prompt_starter + f"\n\nFile: {key}" + f"\n```\n{val}\n```\n"
+
+    prompt_starter = "---"
+
+    return prompt_starter
 
 
 def exit_app(user_message):
@@ -54,11 +96,30 @@ def openrouter_client(user_prompt: str) -> str:
     return decoded_response
 
 
-def coordinator() -> None:
+def session_manager() -> None:
     print("Welcm to ChatBot App!\n\n\n")
     while True:
         # take input from user
         prompt = input("User: ")
+
+        # get file paths from user
+        while True:
+            _file_path = input("Location of file to upload: ")
+            if _file_path == "":
+                break
+            else:
+                file_upload_paths.append(_file_path)
+
+        # handover processing of files to files_hander function
+        file_contents = files_handler(file_upload_paths)
+
+        if len(file_contents) > 0:
+            file_prompt_prefix = file_content_prompt_generator(file_contents=file_contents)
+            prompt = file_prompt_prefix + prompt
+
+        print(file_contents)
+        print(prompt)
+        os._exit(0)
 
         # save prompt in container
         message_history.append({"role": "user", "content": prompt})
@@ -75,4 +136,4 @@ def coordinator() -> None:
 
 
 if __name__ == "__main__":
-    coordinator()
+    session_manager()
